6efec733078e81526b2f7c027bed8402b4a8aab7
Jzavier Timm <jztimm@gmail.com>
2020-06-28 23:01:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,18 +4,22 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 
-def introduction_with_language (name, language)
+def introduction(name)
+  puts "Hi, my name is #{name}."  
+end
+
+
+
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
 end
 
-introduction_with_language ("Jzavier", "JavaScript")
 
-
-def introduction_with_language_optonal (name, language = Ruby)
+def introduction_with_language_optonal(name, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
 end
 
-introduction_with_language_optonal ("Jzavier")
+
 
 
 
