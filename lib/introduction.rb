7af107e1a461ea4760f95404b1fd9c500be33d8d
Jzavier Timm <jztimm@gmail.com>
2020-06-28 22:56:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,7 @@ def introduction_with_language (name, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."  
 end
 
-introduction_with_language_optional("Jzavier")
+introduction_with_language ("Jzavier")
 
 
 
